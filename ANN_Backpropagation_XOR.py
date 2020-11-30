70570b9140a1e4a33f27c670f2eee4de4d26f9f5
Vitor França <vitor.franca@cear.ufpb.br>
2020-11-29 22:54:01 -0300

    finalizacao da adicao do bias

--- ANN_Backpropagation_XOR.py
+++ ANN_Backpropagation_XOR.py
@@ -31,14 +31,14 @@ class Neural():
             (low=-0.1, high=0.1, size=(self.hiddenLayerNeurons, self.outputLayerNeurons)) #2x1
         
         #inicializando os biais
-        self.bias_hidden = np.random.uniform(size=(1, self.hiddenLayerNeurons))
-        self.bias_output = np.random.uniform(size=(1, self.outputLayerNeurons))
+        self.bias_hidden = np.random.uniform(low=0, high= 0.1, size=(1, self.hiddenLayerNeurons))
+        self.bias_output = np.random.uniform(low=0, high= 0.1, size=(1, self.outputLayerNeurons))
 
     #definindo feed forward propagation
     def feedForward(self, x):
         
         #camada escondida
-        self.hidden_net = np.dot(x.T, self.W_hidden) + self.bias_hidden # 1x2 * 2x2 -> 1x2
+        self.hidden_net = np.dot(x, self.W_hidden) + self.bias_hidden # 1x2 * 2x2 -> 1x2
         self.hidden_output = sigmoid(self.hidden_net) # 1x2
 
         #camada de saída
@@ -59,24 +59,29 @@ class Neural():
 
         #atualizando os pesos
         self.W_output += learning_rate * np.dot(self.hidden_output.T, self.delta_output) #2x1 * 1x1 -> 2x1 
-        self.W_hidden += learning_rate * np.dot(x, self.delta_hidden) #2x1 * 1x2 -> 2x2
+        self.W_hidden += learning_rate * np.dot(x.T, self.delta_hidden) #2x1 * 1x2 -> 2x2
+
+        #atualizando os bias
+        self.bias_output += learning_rate * np.sum(self.delta_output, axis=0, keepdims=True)  
+        self.bias_hidden += learning_rate * np.sum(self.delta_hidden, axis=0, keepdims=True)
+        
 
     def train(self, epochs, learning_rate, expected_out, X):
-        for i, x in enumerate(X):
-            for _ in range(epochs):
-                self.output = self.feedForward(x.reshape(2,1))
-                self.feedBackward(x.reshape(2,1), learning_rate, expected_out[i], self.output)
+        #for i, x in enumerate(X):
+        for _ in range(epochs):
+            self.output = self.feedForward(X)
+            self.feedBackward(X, learning_rate, expected_out, self.output)
 
 
 if __name__ == '__main__':
     NN = Neural()
     #treinando a rede:
-    epochs = 15000
-    learning_rate = 0.3
+    epochs = 24000
+    learning_rate = 0.9
     NN.train(epochs, learning_rate, y, X) 
 
     #avaliando a rede treinada:
     output = np.zeros((4,1))
-    for i, x in enumerate(X):
-        output[i] = NN.feedForward(x.reshape(2,1))
-    print (output)    
+    #for i, x in enumerate(X):
+    print(NN.feedForward(X))
+    #print (output)    
