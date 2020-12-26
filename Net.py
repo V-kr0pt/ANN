4d12b99b0e6a756a929a45e40a2bd508440d55f9
V-kr0pt <vitor.franca@cear.ufpb.br>
2020-12-26 15:37:55 -0300

    feedBackward

--- Net.py
+++ Net.py
@@ -9,33 +9,100 @@ class Net:
         self.W = [] #lista de arrays contendo os pesos aleatoriamente iniciados
         self.biases = [] #lista de arrays contendo os bias aleatoriamente iniciados
 
-        self.num_de_camadas = len(neuronios)-1
-        for i in range(self.num_de_camadas):
-            W = np.random.rand(neuronios[i+1], neuronios[i]) #pesos randômicos
+        #contabiliza o número de conexões que existem na rede 
+        self.num_de_conexoes = len(neuronios) - 1 
+        
+        #criando uma lista de 0 até o número de conexões
+        self.conexoes = list(range(self.num_de_conexoes)) 
+        
+        #As conexões existem entre duas camadas, camada e camada+1.        
+        for camada in self.conexoes:
+         
+            #pesos randômicos
+            W = np.random.rand(neuronios[camada+1], neuronios[camada]) 
             W *= 0.1 #garantido pesos inicializados <= |0.1| 
-            self.W.append(W)
+            self.W.append(W) #salva na lista
 
-            biases = np.random.rand(neuronios[i+1]) #bias randômicos            
-            self.biases.append(biases)
+            biases = np.random.rand(neuronios[camada+1]) #bias randômicos            
+            self.biases.append(biases) #salva na lista
+        
 
     def feedForward(self, X):
-        self.X = X   
-        output = self.X #generalizando, a saída do neurônio de entrada é a prórpia entrada 
+        self.X = X 
+
+        #listas para deixar salvo as saídas e as somas das entradas dos neurônios
+        self.output = [] 
+        self.sum_fa = []
         
-        for camada in range(self.num_de_camadas):
-            input_tf = np.dot(output, self.W[camada].T) + self.biases[camada]
-            output = fa.ReLu(input_tf)            
+        #generalizando, a saída do neurônio de entrada é a prórpia entrada 
+        self.output.append(self.X) 
+                
+        for conexao in self.conexoes:
+                    
+            sum_fa =  np.dot(self.output[conexao], self.W[conexao].T) + self.biases[conexao] 
+            output = fa.ReLu(sum_fa)           
+
+            self.sum_fa.append(sum_fa)
+            self.output.append(output)        
+        
+        #retorna a saída da camada de saída
         return output
         
-    def feedBackward(self, target, output, learning_rate):
-        pass
+    def feedBackward(self, target, learning_rate):
+        
+        #invertendo a lista self.conexoes, para indicar o caminho de retropropagação
+        b_conexoes = [self.num_de_conexoes-i-1 for i in self.conexoes]
+
+        #Foi necessário a retirada de 1 unidade de self.num_de_conexoes para que b_conexoes  
+        #representasse uma lista contabilizando o número de conexões com índice iniciando em 0  
+
+        #a camada de saída é a camada posterior a última conexão        
+        camada_de_saida = b_conexoes[0] + 1
+
+        #a variável error da camada de saída 
+        #é dada pela diferença entre o target e o a saída do neurônio       
+        error = target - self.output[camada_de_saida] #erro da camada de saída        
+        
+        for conexao in b_conexoes:  
+            dif_FA = fa.ReLu(self.sum_fa[conexao], diff=True)
+            e = np.dot(error, dif_FA)
+            print(error.shape)
+            print(dif_FA.shape)
+            print(f"{self.output[conexao].shape} \n")          
+
+            
+            #atualização dos pesos 
+            self.W[conexao] = \
+                self.W[conexao] + learning_rate * np.dot(self.output[conexao+1], e)
+            
+
+            #atualização dos biais da camada de saída
+            self.biases[conexao] = \
+                self.biases[conexao]  + learning_rate * np.sum(e, axis=0, keepdims=True)
+
+            error = np.dot(e, self.W[conexao]).sum() #erro da camada escondida
+
+            
+           
+
+        print("Backward sucesso!")
+            
+
+            
+
+            
+            
+            
 
 
 if __name__ == '__main__':
-    NN = Net([2,5,5,1])
+    NN = Net([2,3,3,1])
     X = np.array([[0,0],[0,1],[1,0],[1,1]])
-    output = NN.feedForward(X)
-    print(output)
+    y = np.array([[0],[1],[1],[0]])
+    for i,x in enumerate(X): 
+        print(x)       
+        output = NN.feedForward(x)
+        NN.feedBackward(y[i], 0.1)
 
 
 """
